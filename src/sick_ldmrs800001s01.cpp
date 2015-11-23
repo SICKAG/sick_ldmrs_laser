fc6db8972aeccb4a2edf2be1737e4fa384e02040
Martin Günther <martin.guenther@dfki.de>
2015-11-23 18:29:49 +0100

    Add our ROS driver class as library app

--- src/sick_ldmrs800001s01.cpp
+++ src/sick_ldmrs800001s01.cpp
@@ -35,13 +35,142 @@
  */
 
 #include <iostream>
-#include <sick_ldmrs/manager.hpp>
+
+#include "sick_ldmrs_driver/sick_ldmrs800001s01.hpp"
+
+#include <sick_ldmrs/datatypes/EvalCaseResults.hpp>
+#include <sick_ldmrs/datatypes/EvalCases.hpp>
+#include <sick_ldmrs/datatypes/Fields.hpp>
+#include <sick_ldmrs/datatypes/Measurement.hpp>
+#include <sick_ldmrs/datatypes/Msg.hpp>
+#include <sick_ldmrs/datatypes/Object.hpp>
+#include <sick_ldmrs/datatypes/Scan.hpp>
+
+#include <sick_ldmrs/tools/errorhandler.hpp>
+#include <sick_ldmrs/tools/toolbox.hpp>
+
+
+sick_ldmrs_driver::SickLDMRS::SickLDMRS(Manager *manager)
+  : application::BasicApplication()
+{
+  // TODO MG
+}
+
+void sick_ldmrs_driver::SickLDMRS::setData(BasicData &data)
+{
+  //
+  // Do something with it.
+  //
+  // Here, we just print what we've got.
+  //
+  std::string datatypeStr;
+  std::string sourceIdStr;
+
+  switch (data.getDatatype())
+  {
+  case Datatype_Scan:
+    datatypeStr = "Scan (" + ::toString(((Scan&)data).getNumPoints()) + " points)";
+    {
+      // Print the scan start timestamp (NTP time)
+      Scan* scan = dynamic_cast<Scan*>(&data);
+      const ScannerInfo* info = scan->getScannerInfoByDeviceId(1);
+
+      if (info != NULL)
+      {
+        const Time& time = info->getStartTimestamp();
+        ROS_INFO("LdmrsApp::setData(): Scan start time: %s", time.toString().c_str());
+      }
+
+      // TODO MG: convert to PointCloud, publish
+    }
+    break;
+  case Datatype_Objects:
+    datatypeStr = "Objects (" + ::toString(((ObjectList&)data).size()) + " objects)";
+    break;
+  case Datatype_Fields:
+    datatypeStr = "Fields (" + ::toString(((Fields&)data).getFields().size()) + " fields, " +
+                  ::toString(((Fields&)data).getNumberOfValidFields()) + " of which are valid)";
+    break;
+  case Datatype_EvalCases:
+    datatypeStr = "EvalCases (" + ::toString(((EvalCases&)data).getEvalCases().size()) + " cases)";
+    break;
+  case Datatype_EvalCaseResults:
+    datatypeStr = "EvalCaseResults (" + ::toString(((EvalCaseResults&)data).size()) + " case results)";
+    break;
+  case Datatype_Msg:
+    datatypeStr = "Msg (" + ((Msg&)data).toString() + ")";
+    break;
+  case Datatype_MeasurementList:
+    datatypeStr = "MeasurementList (" + ::toString(((MeasurementList&)data).m_list.size()) + " entries)";
+    break;
+  default:
+    datatypeStr = "(unknown)";
+  }
+
+  sourceIdStr = ::toString(data.getSourceId());
+
+  ROS_INFO("LdmrsApp::setData(): Called with data of type %s from ID %s", datatypeStr.c_str(), sourceIdStr.c_str());
+}
 
 int main(int argc, char **argv)
 {
-  std::cout << "Hello SICK LD-MRS!" << std::endl;
+  ros::init(argc, argv, "sick_ldmrs800001s01");
 
+  // The MRS-App connects to an MRS, reads its configuration and receives all incoming data.
+  // First, create the manager object. The manager handles devices, collects
+  // device data and forwards it to the application(s).
+  ROS_INFO("Creating the manager.");
   Manager manager;
 
-  return 0;
+  // Add the application. As the devices may send configuration data only once
+  // at startup, the applications must be present before the devices are
+  // started.
+  Sourcetype type;
+  std::string name;
+  UINT16 id;
+  bool result = false;
+
+  ROS_INFO("Adding the application SickLDMRS.");
+  name = "Sick LDMRS ROS Driver App";
+  id = 1356;
+
+  application::BasicApplication* app;
+  app = new sick_ldmrs_driver::SickLDMRS(&manager);
+  app->setApplicationName(name);
+
+  result = manager.addApplication(app, id);
+  if (result == false)
+  {
+    ROS_ERROR("Failed to add application %s, aborting!", name.c_str());
+    return EXIT_FAILURE;
+  }
+  ROS_INFO("Application is running.");
+
+  //
+  // Add and run the sensor
+  //
+  // The MRS device could be configured like this:
+  // m_weWantScanData:          true
+  // m_weWantObjectData:        true
+  // m_weWantFieldData:         false
+  // m_weWantScanDataFromSopas: false
+  ROS_INFO("Adding the LDMRS device.");
+  type = Sourcetype_LDMRS;
+  name = "LDMRS-1";
+  id = 1;
+  result = manager.addAndRunDevice(type, name, id);
+  if (result == false)
+  {
+    ROS_ERROR("Failed to add device %s, aborting!", name.c_str());
+    return EXIT_FAILURE;
+  }
+
+  // This loop never ends
+  while (1)
+  {
+    // Sleep 100 ms
+    usleep(100000);
+  }
+
+  return EXIT_SUCCESS;
 }
