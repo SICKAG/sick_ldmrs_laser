bf71658f24c53d2c8b118b71e736a95373867b39
Martin Guenther <martin.guenther@dfki.de>
2016-04-06 18:08:50 +0200

    Add enum Flags to sick_ldmrs_point_type

--- sick_ldmrs_msgs/include/sick_ldmrs_msgs/sick_ldmrs_point_type.h
+++ sick_ldmrs_msgs/include/sick_ldmrs_msgs/sick_ldmrs_point_type.h
@@ -15,6 +15,13 @@ struct SICK_LDMRS_Point
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 } EIGEN_ALIGN16;
 
+enum Flags
+{
+  FlagTransparent = 0x01,    // set if at least one more echo behind this scan point
+  FlagClutter     = 0x02,    // set if scan point is classified as atmospheric noise such as rain, dust, or similar
+  FlagGround      = 0x04,    // set if scan point is classified as ground (only available if corresponding processing is active)
+  FlagDirt        = 0x08     // set if scan point is classified as dirt (usually dirty front screen of sensor housing)
+};
 }
 
 POINT_CLOUD_REGISTER_POINT_STRUCT(sick_ldmrs_msgs::SICK_LDMRS_Point,
